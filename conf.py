a966dafc3493942f05d678aa6f1296e270355508
startdownloadquicken <jnvii87.48@gmail.com>
2026-01-30 17:52:14 +0530

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,15 @@
+project = 'Quicken Desktop Guide'
+author = 'Quicken Desktop Team'
+release = '1.0'
+
+extensions = []
+
+templates_path = []
+exclude_patterns = []
+
+# 🔥 CUSTOM WEBSITE THEME
+html_theme = 'mytheme'
+html_theme_path = ['_theme']
+
+# Static files
+html_static_path = ['_static']
